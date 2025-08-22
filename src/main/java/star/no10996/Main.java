da3186a1ac0797155460a9281db408cd01e2f0cb
chosseang <jng00153@gmail.com>
2025-08-22 23:44:00 +0900

    백준 > 별찍기 > 10996번 문제 풀이

---
+++ src/main/java/star/no10996/Main.java
@@ -0,0 +1,29 @@
+package star.no10996;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+
+		for(int i = 0; i <2*n;i++){
+			for(int j = 0; j < n;j++){
+				if((i+1)%2==0){
+					if((j+1)%2!=0){
+						System.out.print(" ");
+					} else{
+						System.out.print("*");
+					}
+			 	} else{
+					if((j+1)%2!=0){
+						System.out.print("*");
+					} else{
+						System.out.print(" ");
+					}
+				}
+			}
+			System.out.println();
+		}
+	}
+}
