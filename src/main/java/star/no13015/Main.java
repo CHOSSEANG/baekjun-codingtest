596991cf09b02ec3fa2a8fb23c5e7bff1b66bbef
chosseang <jng00153@gmail.com>
2025-08-28 00:38:53 +0900

    백준 > 별찍기 > 13015번 문제 풀이 - 코드 패턴화

--- src/main/java/star/no13015/Main.java
+++ src/main/java/star/no13015/Main.java
@@ -7,104 +7,49 @@ public class Main {
 		Scanner sc = new Scanner(System.in);
 		int N = sc.nextInt();
 
-		// 첫 번째 줄과 마지막 줄
-		String firstLastLine = "";
-		for (int i = 0; i < N; i++) {
-			firstLastLine += "*";
-		}
-		for (int i = 0; i < 2 * N - 3; i++) {
-			firstLastLine += " ";
-		}
-		for (int i = 0; i < N; i++) {
-			firstLastLine += "*";
-		}
+		int width = 4 * N - 3; // 전체 너비
 
-		System.out.println(firstLastLine);
-
-		// 위쪽 절반 (2번째 줄부터 N번째 줄까지)
-		for (int i = 1; i < N; i++) {
-			String line = "";
-
-			// 왼쪽 바깥 공백
-			for (int j = 0; j < i; j++) {
-				line += " ";
+		for (int i = 0; i < 2 * N - 1; i++) {
+			char[] line = new char[width];
+			for (int j = 0; j < width; j++) {
+				line[j] = ' ';
 			}
 
-			line += "*";
-
-			// 왼쪽 안쪽 공백 (N-2개)
-			for (int j = 0; j < N - 2; j++) {
-				line += " ";
-			}
-
-			// 가운데 줄이면 별 하나만
-			if (i == N - 1) {
-				line += "*";
-
-				// 오른쪽 안쪽 공백 (N-2개)
-				for (int j = 0; j < N - 2; j++) {
-					line += " ";
+			if (i == 0 || i == 2 * N - 2) {
+				// 첫째, 마지막 줄
+				for (int j = 0; j < N; j++) {
+					line[j] = '*';
+					line[width - 1 - j] = '*';
 				}
-
-				line += "*";
 			} else {
-				line += "*";
+				int row = (i < N) ? i : 2 * N - 2 - i; // 대칭을 위한 행 번호
 
-				// 중간 공백 (2*(N-1-i)-1개)
-				for (int j = 0; j < 2 * (N - 1 - i) - 1; j++) {
-					line += " ";
+				// 왼쪽 별들
+				line[row] = '*';
+				line[row + N - 1] = '*';
+
+				// 오른쪽 별들
+				if (row != N - 1) {
+					// 가운데 줄이 아닐 때는 별 2개
+					line[width - 1 - row - N + 1] = '*';
+					line[width - 1 - row] = '*';
+				} else {
+					// 가운데 줄일 때는 별 1개
+					line[width - 1 - row] = '*';
 				}
-
-				line += "*";
-
-				// 오른쪽 안쪽 공백 (N-2개)
-				for (int j = 0; j < N - 2; j++) {
-					line += " ";
-				}
-
-				line += "*";
 			}
 
-			System.out.println(line);
+			// 뒤쪽 공백 제거
+			int end = width - 1;
+			while (end >= 0 && line[end] == ' ') {
+				end--;
+			}
+
+			for (int j = 0; j <= end; j++) {
+				System.out.print(line[j]);
+			}
+			System.out.println();
 		}
-
-		// 아래쪽 절반 (N+1번째 줄부터 2N-1번째 줄까지)
-		for (int i = N - 2; i >= 1; i--) {
-			String line = "";
-
-			// 왼쪽 바깥 공백
-			for (int j = 0; j < i; j++) {
-				line += " ";
-			}
-
-			line += "*";
-
-			// 왼쪽 안쪽 공백 (N-2개)
-			for (int j = 0; j < N - 2; j++) {
-				line += " ";
-			}
-
-			line += "*";
-
-			// 중간 공백 (2*(N-1-i)-1개)
-			for (int j = 0; j < 2 * (N - 1 - i) - 1; j++) {
-				line += " ";
-			}
-
-			line += "*";
-
-			// 오른쪽 안쪽 공백 (N-2개)
-			for (int j = 0; j < N - 2; j++) {
-				line += " ";
-			}
-
-			line += "*";
-
-			System.out.println(line);
-		}
-
-		// 마지막 줄
-		System.out.println(firstLastLine);
 
 		sc.close();
 	}
