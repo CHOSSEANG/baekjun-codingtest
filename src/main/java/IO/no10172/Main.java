70e9780350c18c93d0986c84e4493db25b2cfeb7
chosseang <jng00153@gmail.com>
2025-09-05 14:40:54 +0900

    입출력과 사칙연산 > 10172번 문제 풀이

---
+++ src/main/java/IO/no10172/Main.java
@@ -0,0 +1,13 @@
+package IO.no10172;
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("""
+			|\\_/|
+			|q p|   /}
+			( 0 )""\"\\
+			|"^"`    |
+			||_/=\\\\__|
+			""");
+	}
+}
