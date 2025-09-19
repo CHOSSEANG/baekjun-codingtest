069d2809c7fc79c1f5d6ba405cdad446ebc17871
chosseang <jng00153@gmail.com>
2025-09-20 00:29:30 +0900

    반복문 > 10951번 문제 풀이(BufferedReader 방식)

--- src/main/java/loop/no10951/Main.java
+++ src/main/java/loop/no10951/Main.java
@@ -1,17 +1,26 @@
 package loop.no10951;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-		while (sc.hasNextInt()) {
-			int a = sc.nextInt();
-			int b = sc.nextInt();
-			System.out.println(a + b);
+		String input;
+		while ((input = br.readLine()) != null) {
+			String[] tokens = input.split(" ");
+			int a = Integer.parseInt(tokens[0]);
+			int b = Integer.parseInt(tokens[1]);
+			bw.write((a + b) + "\n");
 		}
 
-		sc.close();
+		bw.flush();
+		bw.close();
+		br.close();
 	}
 }
