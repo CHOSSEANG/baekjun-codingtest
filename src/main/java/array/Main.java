554cb7e75bf1116c8c175b6516985ff6fc1e8888
chosseang <jng00153@gmail.com>
2025-09-22 23:19:38 +0900

    1차원 배열 > 10807번 문제 풀이

---
+++ src/main/java/array/Main.java
@@ -0,0 +1,27 @@
+package array;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int arrayLength = sc.nextInt();
+		int result = 0;
+		int[] array = new int[arrayLength];
+
+		for (int i = 0; i < arrayLength; i++) {
+			int input = sc.nextInt();
+			array[i] = input;
+		}
+
+		int targetValue = sc.nextInt();
+
+
+		for(int a : array) {
+			if(a == targetValue) {
+				result++;
+			}
+		}
+		System.out.println(result);
+	}
+}
