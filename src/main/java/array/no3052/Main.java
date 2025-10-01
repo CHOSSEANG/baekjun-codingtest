63037e6e9ffc4b4d5bcbf6ccc8f94efb52567a7d
chosseang <jng00153@gmail.com>
2025-10-01 15:40:10 +0900

    1차원 배열 > 3052번 문제 풀이(HashSet 사용한 방법)

--- src/main/java/array/no3052/Main.java
+++ src/main/java/array/no3052/Main.java
@@ -1,27 +1,22 @@
 package array.no3052;
 
+import java.util.HashSet;
 import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int loop = 10;
-		int divideValue = 42;
-		int[] array = new int[loop];
-		boolean[] boolArray = new boolean[divideValue];
-		int result = 0;
+		HashSet<Integer> remainders = new HashSet<>();
 
-
-		for(int i = 0; i < loop; i++) {
-			array[i] = sc.nextInt();
-			boolArray[array[i]%divideValue] = true;
+		// 10개의 수를 입력받아 42로 나눈 나머지를 HashSet에 저장
+		for (int i = 0; i < 10; i++) {
+			int num = sc.nextInt();
+			remainders.add(num % 42);
 		}
 
-		for (boolean value : boolArray) {
-			if (value) {
-				result++;
-			}
-		}
-		System.out.println(result);
+		// HashSet의 크기 = 서로 다른 나머지의 개수
+		System.out.println(remainders.size());
+
+		sc.close();
 	}
 }
